888b36069553aba544edbda01f74dff7208f9fc5
franco18min <magnagg@gmail.com>
2025-08-28 17:19:19 -0300

    prueba

--- test_hello.py
+++ test_hello.py
@@ -1,15 +1,1 @@
-import requests
-
-URL = "http://127.0.0.1:8001/hello"
-
-print(f"--- Probando el endpoint de 'Hola Mundo' en {URL} ---")
-
-try:
-    response = requests.get(URL)
-    print(f"\nRespuesta recibida con código de estado: {response.status_code}")
-    print("Respuesta (JSON):")
-    print(response.json())
-
-except Exception as e:
-    print(f"\n--- ERROR ---")
-    print(f"No se pudo conectar o procesar la respuesta. Detalle: {e}")
+#imprime las columnas de la tabla repair_order
